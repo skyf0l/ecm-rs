5de8fdf2ba6880caaa9f8b34262ef8de8618ce53
Skyf0l <tom.rorato@epitech.eu>
2023-07-14 16:03:26 +0200

    feat: ecm-sys (low-level bindings to the GMP-ECM library)

---
+++ ecm-sys/src/lib.rs
@@ -0,0 +1,7 @@
+#![doc = include_str!("../README.md")]
+#![allow(deref_nullptr)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+include!("./bindings.rs");
---
+++ ecm-sys/wrapper.h
@@ -0,0 +1,1 @@
+#include <ecm.h>
